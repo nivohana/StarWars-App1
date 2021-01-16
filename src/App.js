1866ef752a9431ef0cdda7fdca05c8cdf9ae510b
Niv Ohana <nivohana01@gmail.com>
2021-01-16 19:13:34 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import {Films} from './components/Films/Films';
+import { Films } from './components/Films/Films';
 
 function App() {
   return (
     <div className="App">
-     <Films />
-     <img className="img1"/>
-     <img className="img2"/>
+      <Films />
+      <img className="img1" />
+      <img className="img2" />
     </div>
 
   );
